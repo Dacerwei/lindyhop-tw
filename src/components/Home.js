06254b272726bf52bb1c12347b0d92884c1897f5
Chinwei.Hsu <chinwei.hsu@kkday.com>
2018-02-20 18:05:25 +0800

    add home brands

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { Segment, Grid, Image, Reveal, Container, Card } from 'semantic-ui-react'
+import _ from 'lodash'
+
 import PORTFOLIO_CONTENTS from '../contents/PortfolioContents'
 import COOP_CONTENTS from '../contents/CoopContents'
+import BRANDS_CONTENTS from '../contents/BrandContents'
+
 import videoProductionIcon from '../statics/image/icon_video_production.svg'
 import performanceIcon from '../statics/image/icon_performance.svg'
 import classesIcon from '../statics/image/icon_classes.svg'
-import _ from 'lodash'
-
 
 const coopIcons = {
     'video': videoProductionIcon,
@@ -19,7 +21,11 @@ export default class Home extends Component {
         return (
             <div>
                 <Segment vertical>main banner</Segment>
-                <Segment vertical>intro banner</Segment>
+                <Segment vertical>
+                    <h2>Introduction | 舞團介紹</h2>
+                    <p>Naughty Swing 搖擺舞團，於 2015 年成立，由6位平均舞齡 10 年、擁有不同街舞背景的舞者所組成。擅長將不同元素融合至 Swing Dance 當中，呈現有別於以往的演出，為目前台灣新生代指標性 Swing 舞團。2016 年發起 I Charleston Taipei 影片拍攝計畫，在 2 周內突破 50 萬人次點閱率，並獲得 10 多家媒體報導，包涵今日台灣、蘋果日報以及TVBS。</p>
+                    <p>Naughty Swing is founded in 2015 by 6 talented Taiwanese dancers, each with over 10 years of experience in street dance. As one of the most iconic dance crews in Taiwan, Naughty Swing is keen to integrate elements from different dance styles and to give unprecedented performances. "I Charleston Taipei" is their first official dance film project launched in 2016, reaching over 500,000 views on YouTube within 2 weeks, and was reported on by over 10 media, including Taiwan Today, Apple Daily and TVBS.</p>
+                </Segment>
                 <Segment vertical>
                     <h2>CO-OP | 合作項目</h2>
                     <p>More than just a dance crew - Naughty Swing specializes in Swing Dance choreography, teaching and performance as well as video filming and editing. If you're amazed by the beauty of Swing Dance, why not pick some options below and work with us now!</p>
@@ -40,77 +46,43 @@ export default class Home extends Component {
                     </Card.Group>
                 </Segment>
                 <Segment vertical>
-                    portfolio banner
-                    <Grid>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            {
-                                _.map(_.filter(PORTFOLIO_CONTENTS, (content, index) => (index % 4 === 0)), (content) => (
-                                    <Container key={content.ID}>
-                                        <Reveal animated='move'>
-                                            <Reveal.Content visible>
-                                                <Image src={content.src} size='large' />
-                                            </Reveal.Content>
-                                            <Reveal.Content hidden>
-                                                <p>{content.title}</p>
-                                            </Reveal.Content>
-                                        </Reveal>
-                                    </Container>
-                                ))
-                            }
-                        </Grid.Column>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            {
-                                _.map(_.filter(PORTFOLIO_CONTENTS, (content, index) => (index % 4 === 1)), (content) => (
-                                    <Container key={content.ID}>
-                                        <Reveal animated='move'>
-                                            <Reveal.Content visible>
-                                                <Image src={content.src} size='large' />
-                                            </Reveal.Content>
-                                            <Reveal.Content hidden>
-                                                <p>{content.title}</p>
-                                            </Reveal.Content>
-                                        </Reveal>
-                                    </Container>
-                                ))
-                            }
-                        </Grid.Column>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            {
-                                _.map(_.filter(PORTFOLIO_CONTENTS, (content, index) => (index % 4 === 2)), (content) => (
-                                    <Container key={content.ID}>
-                                        <Reveal animated='move'>
-                                            <Reveal.Content visible>
-                                                <Image src={content.src} size='large' />
-                                            </Reveal.Content>
-                                            <Reveal.Content hidden>
-                                                <p>{content.title}</p>
-                                            </Reveal.Content>
-                                        </Reveal>
-                                    </Container>
-                                ))
-                            }
-                        </Grid.Column>
-                        <Grid.Column mobile={16} tablet={8} computer={4}>
-                            {
-                                _.map(_.filter(PORTFOLIO_CONTENTS, (content, index) => (index % 4 === 3)), (content) => (
-                                    <Container key={content.ID}>
-                                        <Reveal animated='move'>
-                                            <Reveal.Content visible>
-                                                <Image src={content.src} size='large' />
-                                            </Reveal.Content>
-                                            <Reveal.Content hidden>
-                                                <p>{content.title}</p>
-                                            </Reveal.Content>
-                                        </Reveal>
-                                    </Container>
-                                ))
-                            }
-                        </Grid.Column>
+                    <h2>Portfolio | 作品集</h2>
+                    <Grid textAlign='center'>
+                        {
+                            _.map([0, 1, 2, 3], (column) => {
+                                return (
+                                    <Grid.Column mobile={16} tablet={8} computer={4}>
+                                        {
+                                            _.map(_.filter(PORTFOLIO_CONTENTS, (content, index) => (index % 4 === column)), (content) => (
+                                                <Container key={content.ID}>
+                                                    <Reveal animated='move'>
+                                                        <Reveal.Content visible>
+                                                            <Image src={content.src} size='large' />
+                                                        </Reveal.Content>
+                                                        <Reveal.Content hidden>
+                                                            <p>{content.title}</p>
+                                                        </Reveal.Content>
+                                                    </Reveal>
+                                                </Container>
+                                            ))
+                                        }
+                                    </Grid.Column>
+                                )
+                            })
+                        }
                     </Grid>
                 </Segment>
                 <Segment vertical>classes banner</Segment>
-                <Segment vertical>Brands banner</Segment>
-            </div>
+                <Segment vertical>
+                    <Grid doubling columns={5} textAlign='center'>
+                        {
+                            _.map(BRANDS_CONTENTS, (brand) => (
+                                <Image key={brand.name} src={brand.src} />
+                            ))
+                        }
+                    </Grid>
+                </Segment>
+            </div >
         )
     }
 }
---
+++ src/contents/BrandContents.js
@@ -0,0 +1,82 @@
+const BRANDS_CONTENTS = [
+    {
+        name: '3M',
+        src: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/aboutus%2Fbrands%2Fbrand_3m_edited.svg?alt=media&token=42c09a8a-d85e-4075-88e8-5103c9ef1642',
+        width: 300,
+        height: 100,
+        backgroundColor: 'white',
+    },
+    {
+        name: 'CatchPlay',
+        src: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/aboutus%2Fbrands%2Fbrand_catchplay_edited.svg?alt=media&token=6bebb6e2-2054-4d9f-9018-d4ef70d22b2a',
+        width: null,
+        height: null,
+        backgroundColor: null,
+    },
+    {
+        name: 'Legacy',
+        src: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/aboutus%2Fbrands%2Fbrand_legacy_edited.svg?alt=media&token=a3c413eb-7202-47c3-8e17-b1b0818f2732',
+        width: null,
+        height: null,
+        backgroundColor: null,
+    },
+    {
+        name: 'Lee',
+        src: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/aboutus%2Fbrands%2Fbrand_lee_edited.svg?alt=media&token=b7d1e638-ef43-4bb2-a459-ab8d7ed6c3e4',
+        width: null,
+        height: null,
+        backgroundColor: null,
+    },
+    {
+        name: 'Coretronic',
+        src: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/aboutus%2Fbrands%2Fbrand_coretronic_edited.svg?alt=media&token=9ac1e271-affd-4dd1-b5ae-734c49925d46',
+        width: null,
+        height: null,
+        backgroundColor: 'white',
+    },
+    {
+        name: 'GoldenHorse',
+        src: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/aboutus%2Fbrands%2Fbrand_goldenhorse_edited.svg?alt=media&token=27627ef1-622e-44db-a553-1ef89d3dbe25',
+        width: null,
+        height: null,
+        backgroundColor: null,
+    },
+    {
+        name: 'MOCA',
+        src: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/aboutus%2Fbrands%2Fbrand_moca_edited.svg?alt=media&token=a5699df6-c45c-4564-99a5-5cd5426f6200',
+        width: null,
+        height: null,
+        backgroundColor: '#F6F6F6',
+    },
+    {
+        name: 'TEDxCCU',
+        src: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/aboutus%2Fbrands%2Fbrand_tedccu_edited.svg?alt=media&token=1f5ade5d-ef9a-4ce8-a029-48442f1e580c',
+        width: null,
+        height: null,
+        backgroundColor: 'white',
+    },
+    {
+        name: 'LionTravel',
+        src: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/aboutus%2Fbrands%2Fbrand_liontravel_edited.svg?alt=media&token=2c47031c-0073-467a-9995-9a8e99d5013f',
+        width: null,
+        height: null,
+        backgroundColor: null,
+    },
+    {
+        name: 'TTT',
+        src: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/aboutus%2Fbrands%2Fbrand_ttt_edited.svg?alt=media&token=3bbe53fb-4942-44db-82f1-3c16a92cfa00',
+        width: null,
+        height: null,
+        backgroundColor: 'white',
+    },
+    {
+        name: 'superfm',
+        src: 'https://firebasestorage.googleapis.com/v0/b/naughty-swing-official-website.appspot.com/o/aboutus%2Fbrands%2Fbrand_superfm_edited.svg?alt=media&token=ee63e77e-6991-42ab-8aa1-d24b81e4fa49',
+        width: null,
+        height: null,
+        backgroundColor: null,
+    }
+
+];
+
+export default BRANDS_CONTENTS;
